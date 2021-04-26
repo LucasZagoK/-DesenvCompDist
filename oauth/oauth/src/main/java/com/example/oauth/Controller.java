6ab1009353d44834ee9f9fdd45603815dc454045
Lucas Zago <zagolucas2k@gmail.com>
2021-04-26 19:38:36 -0300

    Nome do user logado na tela

--- oauth/oauth/src/main/java/com/example/oauth/Controller.java
+++ oauth/oauth/src/main/java/com/example/oauth/Controller.java
@@ -1,5 +1,10 @@
 package com.example.oauth;
 
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +16,9 @@ public class Controller {
 		return "Hello world";
 	}
 	
-	@GetMapping("/restricted")
-	public String restricted() {
-		return "You are able to see this if you are logged";
-	}
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+	
 }
